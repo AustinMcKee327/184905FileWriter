a146eeceebfa23944636b3db39898db54073faa8
AustinMcKee327 <47399911+AustinMcKee327@users.noreply.github.com>
2019-03-20 09:58:22 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _184905FileWriterExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAppend_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(txtName.Text, true);
+                sw.WriteLine(txtInput.Text);
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnWrite_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(txtName.Text);
+                sw.WriteLine(txtInput.Text);
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSquare_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(txtName.Text);
+                //sw.WriteLine(txtInput.Text);
+                //loop of numbers from 1 to input number
+                int theNumber;
+                int.TryParse(txtInput.Text, out theNumber);
+                for (int j = 1; j <= theNumber; j++)
+                {
+                    sw.WriteLine(j + "\t" + j * j);
+                }
+                
+                    //after each number add tab
+                    //after each tab add squared number
+
+
+                    sw.Flush();
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
